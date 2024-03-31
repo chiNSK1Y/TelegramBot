9540c0b66ab03070328f90b8805c365727231f14
chiNSK1Y <panarin-chin@mail.ru>
2024-03-31 09:34:08 +0300

    createsimpledoc

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -25,6 +27,9 @@ namespace TelegramBot
 
         //Ключ Api
         const string apiTelegramKey = "7120360775:AAEouZykT2AhZclMFTjwChWMK6pMwmiXGRA";
+
+        //Ожидание документа
+        private static Dictionary<long, bool> expectingDocument = new Dictionary<long, bool>(); // Хранение состояний для каждого пользователя
 
         static async Task Main()
         {
@@ -99,6 +104,9 @@ namespace TelegramBot
         //Обработчик для типа сообщения Message
         private static async Task UpdateMessageHandler(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
+            //Интеграция с directum rx
+            DirectumRX directumRX = new DirectumRX();
+
             //Сообщение
             var message = update.Message;
             //От кого
@@ -110,9 +118,10 @@ namespace TelegramBot
             //На консоле отобразим что пришло и от кого
             Console.WriteLine($"{userFrom.FirstName} ({userFrom.Id}) написал сообщение: {message.Text}");
 
-            //Команда help
+            //Передан текст и там указана команда
             if (!string.IsNullOrEmpty(message.Text) && message.Text.StartsWith("/"))
             {
+                //Команда помощника
                 if (message.Text == "/help")
                 {
                     await botClient.SendTextMessageAsync(
@@ -126,9 +135,6 @@ namespace TelegramBot
                 //Команда /start
                 if (message.Text == "/start")
                 {
-                    //Интеграция с directum rx
-                    DirectumRX directumRX = new DirectumRX();
-
                     //Проверяем наличие пользака в Directum RX
                     bool isUserFound = await directumRX.CheckRegisteredUser(update.Message.From.Id);
 
@@ -144,8 +150,8 @@ namespace TelegramBot
 
                                         new InlineKeyboardButton[] // тут создаем массив кнопок
                                         {
-                                            InlineKeyboardButton.WithUrl("Справка", "https://habr.com/"), //TODO показать потом справку
-                                            InlineKeyboardButton.WithCallbackData("Какое-то действие", "action"),
+                                            InlineKeyboardButton.WithCallbackData("Создать простой документ", "CreateSimpleDocument"),
+                                            InlineKeyboardButton.WithCallbackData("Создать заявление на отпуск", "CreateVacationDocument"),
                                         },
                             });
 
@@ -179,13 +185,82 @@ namespace TelegramBot
                     return;
                 }
 
+                //Команда отмены
+                if (message.Text == "/cancel")
+                {
+                    //Очистка
+                    expectingDocument.Remove(chat.Id);
+                    
+                    return;
+                }
+
                 //Возвращаем что команда не определена
                 await botClient.SendTextMessageAsync(
                                 chat.Id,
-                                "Не опознанная команда",
-                                replyToMessageId: message.MessageId // по желанию можем поставить этот параметр, отвечающий за "ответ" на сообщение
+                                "Не опознанная команда"//,
+                                //replyToMessageId: message.MessageId // по желанию можем поставить этот параметр, отвечающий за "ответ" на сообщение
                             );
                 return;
+            }
+
+            //Ожидание документа
+            if (expectingDocument.Where(x => x.Key == chat.Id).Any())
+            {
+                //Если ожидается документ
+                if (message.Document == null)
+                {
+                    await botClient.SendTextMessageAsync(
+                                        chat.Id,
+                                        "Ожидается прикрипление файла. Если хотите отменить операцию напишите команду /cancel",
+                                        replyToMessageId: message.MessageId // по желанию можем поставить этот параметр, отвечающий за "ответ" на сообщение
+                                    );
+
+                    return;
+                }
+
+                //Обработка документа и его создание
+                if (message.Document != null)
+                {
+
+                    var exceptDocument = expectingDocument.Where(x => x.Key == chat.Id)
+                        .Select(x => x.Value)
+                        .FirstOrDefault();
+
+                    //Если документ ожидается, то овечаем пользователю
+                    if (exceptDocument)
+                    {
+                        await botClient.SendTextMessageAsync(
+                                        chat.Id,
+                                        "Файл получен",
+                                        replyToMessageId: message.MessageId // по желанию можем поставить этот параметр, отвечающий за "ответ" на сообщение
+                                    );
+
+                        //Скачали и записали в файл
+                        var fileInfo = await botClient.GetFileAsync(message.Document.FileId);
+                        var filePath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\{message.Document.FileName}";
+
+                        using (var stream = System.IO.File.OpenWrite(filePath))
+                        {
+                            await botClient.DownloadFileAsync(fileInfo.FilePath, stream);
+                        }
+
+                        //Передаем в метод создания документа
+                        //var documentIdDRX = await directumRX.CreateSimpleDocument(userFrom.Id, filePath);
+                        var documentIdDRX = 0;
+
+                        await botClient.SendTextMessageAsync(
+                                                            chat.Id,
+                                                            $"Документ (ссылка - {documentIdDRX}) создан в Directum RX.",
+                                                            replyToMessageId: message.MessageId // по желанию можем поставить этот параметр, отвечающий за "ответ" на сообщение
+                                                        );
+                        return;
+
+                    }
+                    //Очистка
+                    expectingDocument.Remove(chat.Id);
+
+                    return;
+                }
             }
         }
 
@@ -211,8 +286,8 @@ namespace TelegramBot
                     //Отправляем ответ что регистрация завершена и нужно выполнить команду /start
                     await botClient.SendTextMessageAsync(
                                     message.Chat.Id,
-                                    "Регистрация завершена, выполните команду /start для просмотра доступны действий.",
-                                    replyToMessageId: message.MessageId // по желанию можем поставить этот параметр, отвечающий за "ответ" на сообщение
+                                    "Регистрация завершена, выполните команду /start для просмотра доступны действий."//,
+                                    //replyToMessageId: message.MessageId // по желанию можем поставить этот параметр, отвечающий за "ответ" на сообщение
                                 );
                 }
                 else
@@ -220,12 +295,25 @@ namespace TelegramBot
                     //Отправляем ответ что регистрация завершена и нужно выполнить команду /start
                     await botClient.SendTextMessageAsync(
                                     message.Chat.Id,
-                                    "Регистрация завершилась с ошибкой, попробуйте позже!",
-                                    replyToMessageId: message.MessageId // по желанию можем поставить этот параметр, отвечающий за "ответ" на сообщение
+                                    "Регистрация завершилась с ошибкой, попробуйте позже!"//,
+                                    //replyToMessageId: message.MessageId // по желанию можем поставить этот параметр, отвечающий за "ответ" на сообщение
                                 );
                 }
                 
                 return;
+            }
+
+            //Создание простого документа
+            if (update.CallbackQuery.Data == "CreateSimpleDocument")
+            {
+                await botClient.AnswerCallbackQueryAsync(update.CallbackQuery.Id);
+
+                await botClient.SendTextMessageAsync(
+                                chatId: message.Chat.Id,
+                                text: "Приложите документ. Название документа в Directum RX будет взято из названия файла."
+                            );
+                //Ожидаем от пользователя документ
+                expectingDocument.Add(message.Chat.Id, true);
             }
 
             return;
